d26e6b0a913aa78a86cf6fa5a4f821f0d9cfe2f4
Federico N <nfederico@live.com>
2020-08-23 01:54:28 -0300

    Desfio09 finalizado

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import { request } from 'express';
 import Order from '../infra/typeorm/entities/Order';
 import IOrdersRepository from '../repositories/IOrdersRepository';
 
@@ -20,13 +21,65 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+    if (!customerExists) {
+      throw new AppError('Could not find a customer with the given id');
+    }
+    const existentProducts = await this.productsRepository.findAllById(
+      products,
+    );
+
+    if (!existentProducts.length) {
+      throw new AppError('Could not find any product with the given id');
+    }
+    const existentProductsIds = existentProducts.map(product => product.id);
+    const inexistentProducts = products.filter(
+      product => !existentProductsIds.includes(product.id),
+    );
+    if (inexistentProducts.length) {
+      throw new AppError(`Could not find product ${inexistentProducts[0].id}`);
+    }
+    const findProductsWithNoQtyAvailable = products.filter(
+      product =>
+        existentProducts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+    if (findProductsWithNoQtyAvailable.length) {
+      throw new AppError(
+        `The qty  ${findProductsWithNoQtyAvailable[0].quantity} is not available for ${findProductsWithNoQtyAvailable[0].id}`,
+      );
+    }
+    const serializedProduct = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existentProducts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: serializedProduct,
+    });
+
+    const { order_products } = order;
+
+    const orderedProductsQty = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existentProducts.filter(p => p.id === product.product_id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQty);
+    return order;
   }
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 
 import Product from '../infra/typeorm/entities/Product';
 import IProductsRepository from '../repositories/IProductsRepository';
+import ProductsRepository from '../infra/typeorm/repositories/ProductsRepository';
 
 interface IRequest {
   name: string;
@@ -13,10 +14,24 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
-    // TODO
+    const productExists = await this.productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError('One Product is already registered with same name');
+    }
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
